d30666f8f1418bf57ac4d15e691136f92563799d
andylamax <andylamax@programmer.net>
2021-05-02 15:17:33 +0300

    updated gradle config

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -3,7 +3,7 @@ object vers {
     val nexus_staging = "0.22.0"
 
     object asoft {
-        val live = "0.0.20"
+        val live = "0.0.30"
         val test = "1.1.30"
         val builders = "1.3.30"
         val expect = "0.0.30"
