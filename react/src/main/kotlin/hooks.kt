7361976b7888c0cec6b6cf0e1b16a67cd3f16fbe
andylamax <andylamax@programmer.net>
2023-01-03 06:07:19 +0300

    Made useNullableLive react when props change

--- react/src/main/kotlin/hooks.kt
+++ react/src/main/kotlin/hooks.kt
@@ -1,18 +1,28 @@
-@file:Suppress("NON_EXPORTABLE_TYPE")
+@file:JsExport @file:Suppress("NON_EXPORTABLE_TYPE")
 
 import koncurrent.Executor
-import koncurrent.Executors
 import koncurrent.SynchronousExecutor
 import live.Live
 import live.WatchMode
 import live.watch
+import react.useEffect
 import react.useEffectOnce
 import react.useState
 
-@JsExport
+fun <S> useNullableLive(live: Live<S>?): S? = useNullableLiveWithExecutor(live, SynchronousExecutor)
+
+fun <S> useNullableLiveWithExecutor(live: Live<S>?, executor: Executor): S? {
+    var state by useState(live?.value)
+    useEffect(live, executor) {
+        val mode = if (state == null) WatchMode.Eagerly else WatchMode.Casually
+        val watcher = live?.watch(mode = mode, executor) { state = it }
+        if (watcher != null) cleanup { watcher.stop() }
+    }
+    return state
+}
+
 fun <S> useLive(live: Live<S>) = useLiveWithExecutor(live, SynchronousExecutor)
 
-@JsExport
 fun <S> useLiveWithExecutor(live: Live<S>, executor: Executor): S {
     var state by useState(live.value)
     useEffectOnce {
--- react/src/main/kotlin/live/LiveStateUtils.kt
+++ react/src/main/kotlin/live/LiveStateUtils.kt
@@ -3,5 +3,8 @@ package live
 import koncurrent.Executor
 import koncurrent.SynchronousExecutor
 import useLiveWithExecutor
+import useNullableLiveWithExecutor
 
 inline fun <S> Live<S>.watchAsState(executor: Executor = SynchronousExecutor) = useLiveWithExecutor(this, executor)
+
+inline fun <S> Live<S>?.watchAsState(executor: Executor = SynchronousExecutor) = useNullableLiveWithExecutor(this, executor)
